d4e48ffccc1b66be871d6b5dd44d25af9fb96270
chislov <chislov.vu@kvant-telecom.ru>
2022-11-16 11:38:53 +0300

    final Router class, autoload classes, redirect routing

--- public/index.php
+++ public/index.php
@@ -1,14 +1,24 @@
 <?php
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
-require '../app/controllers/Main.php';
-require '../app/controllers/Posts.php';
-require '../app/controllers/PostsNew.php';
+
+define('WWW', __DIR__);
+define('CORE', dirname(__DIR__) . '/vendor/core');
+define('ROOT', dirname(__DIR__));
+define('APP', dirname(__DIR__) . '/app');
+
+// autoload classes
+spl_autoload_register(function($class){
+    $file = APP . "/controllers/$class.php";
+    if (is_file($file)) require_once $file;
+});
 
 $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
-// default rules
-// regexp: [a-z-]+ => Латиница и знак тире а также или более символов
+// pages/index -> posts/index
+Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'posts']);
+
+// default routs
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 debug(Router::getRoutes());
