05df0662c085202819eee5a35b2898908efe1d0d
chislov <chislov.vu@kvant-telecom.ru>
2022-11-16 08:57:36 +0300

    Router mathes with regular expressions

--- public/index.php
+++ public/index.php
@@ -4,9 +4,15 @@ require '../vendor/libs/functions.php';
 
 $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+// Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+// Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+// Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+// default rules
+// regexp: [a-z-]+ - Латиница + знак тире или более символов
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)');
+
 debug(Router::getRoutes());
 
-Router::matchRoute($query) ? debug(Router::getRoute()) : debug("404");
+if (Router::matchRoute($query)) { debug(Router::getRoute()); } else { echo "404"; }
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -5,31 +5,36 @@
 */
 class Router {
 
-    protected static $routes = [];
-    protected static $route = [];
+    protected static $routes = [];      // таблица маршрутов
+    protected static $route = [];       // текущий маршрут
 
+    // Добавление в таблицу маршрутов
     public static function add($regexp, $route = [])
     {
         self::$routes[$regexp] = $route;
     }
 
+    // Получить все маршруты
     public static function getRoutes()
     {
         return self::$routes;
     }
 
+    // Получить текущий маршрут
     public static function getRoute()
     {
         return self::$route;
     }
 
+    // Найти совпадение $url по таблице маршрутов
     public static function matchRoute($url)
     {
         foreach (self::$routes as $pattern => $route)
         {
-            if ($url == $pattern)
+            if (preg_match("#$pattern#i", $url, $matches))
             {
-                self::$route = $route;
+                debug($matches);
+                self::$route = ['controller' => $matches['controller'], 'action' => $matches['action']];
                 return true;
             }
         }
